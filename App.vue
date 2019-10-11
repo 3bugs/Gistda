7fc6b08804eef9194eac1bfae6328d17046a6437
Promlert Lovichit <promlert@gmail.com>
2019-10-11 17:04:11 +0700

    Initial commit

---
+++ App.vue
@@ -0,0 +1,35 @@
+<template>
+    <view class="container">
+        <text class="text-color-primary">{{message}}</text>
+        <button v-bind:title="message" v-bind:on-press="handleButtonPress"/>
+    </view>
+</template>
+
+<script>
+    export default {
+        data: () => {
+            return {
+                message: 'Hello Vue Native',
+            };
+        },
+        methods: {
+            handleButtonPress: function() {
+                this.message = 'Promlert';
+                alert('Button pressed');
+            },
+        }
+    }
+</script>
+
+<style>
+    .container {
+        background-color: white;
+        align-items: center;
+        justify-content: center;
+        flex: 1;
+    }
+
+    .text-color-primary {
+        color: blue;
+    }
+</style>
