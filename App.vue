5ec030e2d9487f2d973fe99f9875bd105eae74c6
Promlert Lovichit <promlert@gmail.com>
2019-10-11 17:20:55 +0700

    แอพ BMI Calculator

--- App.vue
+++ App.vue
@@ -1,7 +1,18 @@
 <template>
     <view class="container">
-        <text class="text-color-primary">{{message}}</text>
-        <button v-bind:title="message" v-bind:on-press="handleButtonPress"/>
+        <text class="title">BMI Calculator</text>
+
+        <text>ส่วนสูง (cm)</text>
+        <text-input v-model="height"
+                    :style="{height: 40, width: 150, borderColor: 'gray', borderWidth: 1, marginBottom: 10}"/>
+
+        <text>น้ำหนัก (kg)</text>
+        <text-input v-model="weight"
+                    :style="{height: 40, width: 150, borderColor: 'gray', borderWidth: 1, marginBottom: 20}"/>
+
+        <!--<text class="text-color-primary">{{bmi}}</text>-->
+        <button title="คำนวณ BMI" v-bind:on-press="handleButtonPress"
+                :style="{height: 40, width: 200}"/>
     </view>
 </template>
 
@@ -9,13 +20,15 @@
     export default {
         data: () => {
             return {
-                message: 'Hello Vue Native',
+                bmi: '',
+                height: '',
+                weight: '',
             };
         },
         methods: {
             handleButtonPress: function() {
-                this.message = 'Promlert';
-                alert('Button pressed');
+                let result =  this.weight / Math.pow(this.height / 100, 2);
+                alert('ค่า BMI ของคุณคือ ' + result);
             },
         }
     }
@@ -29,7 +42,15 @@
         flex: 1;
     }
 
+    .title {
+        color: #666;
+        font-size: 20;
+        font-weight: bold;
+        margin-bottom: 30;
+    }
+
     .text-color-primary {
         color: blue;
+        margin-bottom: 20;
     }
 </style>
