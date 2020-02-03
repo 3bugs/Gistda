8aedf26c9c62e9610864c059b7321c0194ca158b
Promlert Lovichit <promlert@gmail.com>
2020-02-03 11:32:34 +0700

    setup lib (google maps) บน xcode

--- ios/gistda/AppDelegate.m
+++ ios/gistda/AppDelegate.m
@@ -10,11 +10,14 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyC1e9L1eA1YyOhsKW4-BhhwHD2fgtqWnak"];
+
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"gistda"
